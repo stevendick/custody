f47e4fb55b4d2a832e5e61a79b7918ee110709a3
Steven Dick <steven@stevendick.me.uk>
2012-09-14 17:07:45 +0200

    initial version with Angular using Twitter bootstrap

--- web-app/app/js/app.js
+++ web-app/app/js/app.js
@@ -5,6 +5,7 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/landing', {templateUrl: 'partials/landing.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/trades', {templateUrl: 'partials/trades.html', controller: MyCtrl2});
+    $routeProvider.when('/reports', {templateUrl: 'partials/reports.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/landing'});
   }]);
