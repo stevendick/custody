bd58bd5c89b1048c974b22c95a40ca344ea72ebe
Steven Dick <steven@stevendick.me.uk>
2012-09-18 17:46:42 +0200

    Escape the table name used for user class
    Escape the table name used for user class as it is a reserved keyword in
    SQL Server

--- grails-app/domain/ch/hedgesphere/User.groovy
+++ grails-app/domain/ch/hedgesphere/User.groovy
@@ -3,10 +3,14 @@ package ch.hedgesphere
 class User {
     String username
     String passwordHash
-    
+
     static hasMany = [ roles: Role, permissions: String ]
 
     static constraints = {
         username(nullable: false, blank: false, unique: true)
     }
+
+    static mapping = {
+        table '`user`'
+      }
 }
