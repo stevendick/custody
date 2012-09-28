67d0e0b889018bcb10588ce51b5a1cf522fdc156
Steven Dick <steven@stevendick.me.uk>
2012-09-28 17:28:12 +0200

    add Jasmine-based sample testing

---
+++ web-app/app/js/helloworld.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sdick
+ * Date: 28/09/12
+ * Time: 16:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+function helloworld(name) {
+    return 'hello ' + name;
+}
---
+++ web-app/test/helloworldTest.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sdick
+ * Date: 28/09/12
+ * Time: 16:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+describe('hello world', function(){
+    it('returns hello + name', function(){
+        expect(helloworld('bob')).toEqual('hello bobby');
+    });
+});
